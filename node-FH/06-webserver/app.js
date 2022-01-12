a7c4ffb031a817a0b6b3b5a584db1e0d47bed217
Iara Baya <iarabayav@gmail.com>
2022-01-12 20:03:28 -0300

    web server basics start

---
+++ node-FH/06-webserver/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express();
+const port = 8080;
+
+//middleware, servir contenido estatico
+app.use( express.static('public') );
+
+// app.get('/', function (req, res) {
+//   res.send('Home Page')
+// });
+
+app.get('/hola-mundo', function (req, res) {
+  res.send('Hello World desde su ruta');
+});
+
+app.get('*', function (req, res) {
+  res.sendFile(__dirname + '/public/404.html');
+  // res.send('404 | Page not found')
+});
+
+app.listen(port, ()=>{
+  console.log(`App listening at http://localhost:${port}`);
+})
