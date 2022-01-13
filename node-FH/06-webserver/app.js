8cb81bd6d9be8cd4c056b34f70fa954255de4f87
Iara Baya <iarabayav@gmail.com>
2022-01-13 13:23:25 -0300

    replace html with handlebars templates and partials

--- node-FH/06-webserver/app.js
+++ node-FH/06-webserver/app.js
@@ -1,18 +1,48 @@
 const express = require('express')
+const hbs = require('hbs');
+
 const app = express();
 const port = 8080;
+
+//Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials( __dirname + '/views/partials', function (err) {});
 
 //middleware, servir contenido estatico
 app.use( express.static('public') );
 
+app.get('/', function (req, res) {
+  res.render('home', {
+    nombre: 'Iara',
+    titulo: 'Node Course'
+  });
+});
+
 app.get('/generic', function (req, res) {
-  res.sendFile(__dirname + '/public/generic.html');
+  res.render('generic', {
+    nombre: 'Iara',
+    titulo: 'Node Course'
+  });
 });
 
 app.get('/elements', function (req, res) {
-  res.sendFile(__dirname + '/public/elements.html');
+  res.render('elements', {
+    nombre: 'Iara',
+    titulo: 'Node Course'
+  });
 });
 
+//URLs con archivos html
+// app.get('/generic', function (req, res) {
+//   res.sendFile(__dirname + '/public/generic.html');
+// });
+
+// app.get('/elements', function (req, res) {
+//   res.sendFile(__dirname + '/public/elements.html');
+// });
+
+
+//URLs puras con informacion cruda o html
 // app.get('/', function (req, res) {
 //   res.send('Home Page')
 // });
