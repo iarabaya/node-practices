5fb94b9333b4d40d830ebf1c360745e2d05a37c1
iara baya <iarabayav@gmail.com>
2022-02-25 12:00:26 -0300

    basic socket project

---
+++ node-FH/10-socket-fila/sockets/controller.js
@@ -0,0 +1,18 @@
+const socketController = (socket) =>{
+ 
+  socket.on('disconnect', () => {
+      console.log('disconected client', socket.id);
+  })
+
+  socket.on('send-msg', ( payload, callback ) => {
+      // console.log('message from client received ', payload);
+      const id = 123456;
+      callback( {id, date: new Date().getDate() });
+      
+      socket.broadcast.emit('send-message', payload);
+  })
+}
+
+module.exports = {
+  socketController
+}
